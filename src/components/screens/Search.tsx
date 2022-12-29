be0b1cc870a0fd71bedbf2d1c06a175c61a265be
signkj <signkj@nate.com>
2022-12-29 11:39:59 +0900

    search page ui plus icon

--- src/components/screens/Search.tsx
+++ src/components/screens/Search.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, ScrollView} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
 import React from 'react';
 import SearchBox from '../screenComponents/SearchBox';
 import SearchContents from '../screenComponents/SearchContents';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 //test
 const Search = () => {
@@ -17,6 +19,10 @@ const Search = () => {
       <ScrollView showsVerticalScrollIndicator={false}>
         <SearchBox />
         <SearchContents />
+        <TouchableOpacity
+          style={{margin: 25, justifyContent: 'center', alignItems: 'center'}}>
+          <AntDesign name="pluscircleo" style={{fontSize: 40, opacity: 0.5}} />
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
