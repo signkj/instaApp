2a73df54490d5e661cfd383a94b4eb0d42cf1568
signkj <signkj@nate.com>
2023-01-01 18:13:03 +0900

    reels tile, profile2

--- src 복사본/navigator/navigator.type.ts
+++
@@ -1,9 +0,0 @@
-import {StackNavigationProp} from '@react-navigation/stack';
-
-export type AppStackParamList = {
-  Status: {name: string; image: string};
-};
-export type StatusNavigationProps = StackNavigationProp<
-  AppStackParamList,
-  'Status'
->;
